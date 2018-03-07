3378b08729b88262c38a713cc146d7efdd260c51
Nathan Smith <nrsmith012@gmail.com>
2018-03-07 15:24:35 -0800

    fixed minOfRotArr function

--- ch7_min_of_rotated_sorted_arr.js
+++ ch7_min_of_rotated_sorted_arr.js
@@ -1,12 +1,22 @@
-module.exports = function minOfRotArr(arr){
+module.exports = function minOfRotArr (arr) {
     let idx = 0;
     let jdx = arr.length - 1;
     if (arr[idx] < arr[jdx]){ //tells us it must be rotated by 0
         return arr[idx];   //so we return 0th elem.
     }
+    let mPoint;
     while ( (arr[idx] > arr[jdx]) && (idx < jdx) ) {
-        let mPoint = Math.floor((idx+jdx)/2);
-        if (arr[mPoint] <= arr[idx]) {
+        console.log(`
+        
+        ____________
+        loop begin
+        mPoint ${mPoint}
+        jdx ${jdx}; arr[jdx] ${arr[jdx]}
+        idx ${idx}; arr[idx] ${arr[idx]}
+        ____________
+        `);
+        mPoint = Math.floor((idx+jdx)/2); //declared outside loop for now        
+        if (arr[mPoint] < arr[jdx]) {
             jdx = mPoint;
         } else {
             if (arr[jdx-1] < arr[jdx]) {
@@ -24,6 +34,16 @@ module.exports = function minOfRotArr(arr){
                 return arr[idx+1];
             }
         }
+        console.log(`
+        
+        ____________
+        loop end
+        mPoint ${mPoint}
+        jdx ${jdx}; arr[jdx] ${arr[jdx]}
+        idx ${idx}; arr[idx] ${arr[idx]}
+        ____________
+        `);
     }
     return arr[jdx];
 }
+
