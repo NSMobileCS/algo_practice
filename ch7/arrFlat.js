76e44127b983cba7db64f4d72d7e2975f4c8f4d7
Nathan Smith <nrsmith012@gmail.com>
2018-03-08 19:26:39 -0800

    cleanup edits, 1 more test

--- ch7/arrFlat.js
+++ ch7/arrFlat.js
@@ -9,16 +9,24 @@ function arrFlat (arr, rc) {
             return [arr.map( (t) => arrFlat(t, rc++) )];
         }
     } else {
-        return arr;
+        if (rc > 0) {
+            return arr;
+        } else {
+            return [arr];
+        }
+
     }
 }
 
 let a1 = [2, [[4, 6], [8], 10]];
 let a2 = [2, [4, 6, 8, 10]];
-
+let a3 = [['cat', ['flat', 'pat']], ['quant', [['aardvarks'], 'application'], ['azerbaijan']]];
 
 console.log(`a1 = ${a1}
 arrFlat(a1) => ${arrFlat(a1)}`);
-
+console.log("#=#=#=#=#=#=#=#=#");
 console.log(`a2 = ${a2}
 arrFlat(a2) => ${arrFlat(a2)}`);
+console.log("#=#=#=#=#=#=#=#=#");
+console.log(`a3 = ${a3}
+arrFlat(a3) => ${arrFlat(a3)}`);
