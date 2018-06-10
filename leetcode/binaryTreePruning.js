8131f8135b31c2538fe6250f3ede435bfda13e45
Nathan Smith <nrsmith012@gmail.com>
2018-06-10 13:05:14 -0700

    on a leetcode roll

--- leetcode/binaryTreePruning.js
+++ leetcode/binaryTreePruning.js
@@ -6,9 +6,33 @@
  * }
  */
 /**
-//  * @param {TreeNode} root
-//  * @return {TreeNode}
-//  */
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+var pruneTree = function(root) {
+  let legs = 2;
+  if (root.left) {
+    root.left = pruneTree(root.left);
+  } else {
+    legs -= 1;
+  }
+  if ( root.right ) {
+    root.right = pruneTree(root.right);
+  } else {
+    legs -= 1;
+    // now if legs == 0, we're on a terminal node.
+  }
+  if (  (legs == 0) || ( (root.right == null && root.left == null) )  ) {
+    if (root.val == 0) return null;
+  }
+  return root;
+};
+
+
+
+
+
 
 // hasNonZerVal = function (root) {
 //   if (root.val && root.val == 1) return true;
@@ -21,18 +45,18 @@
 //   return false;
 // }
 
-var pruneTree = function(root) {
-  console.log(root);
-  let mustKeep = false;
-  if ( root.left ) {
-    root.left = pruneTree(root.left);
-  } else {
-    root.left = null;
-  }
-  if ( root.right ) {
-    root.right = pruneTree(root.right);
-  } else {
-    root.right = null;
-  }
-  return root;
-};
+// var pruneTree = function(root) {
+//   console.log(root);
+//   let mustKeep = false;
+//   if ( root.left ) {
+//     root.left = pruneTree(root.left);
+//   } else {
+//     root.left = null;
+//   }
+//   if ( root.right ) {
+//     root.right = pruneTree(root.right);
+//   } else {
+//     root.right = null;
+//   }
+//   return root;
+// };
