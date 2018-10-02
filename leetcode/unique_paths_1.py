bff0773a50f4c0ee538556b7e3444375d58c712f
Nathan Smith <nrsmith012@gmail.com>
2018-10-02 13:11:05 -0700

    unique_paths_1 passes after optimization

--- leetcode/unique_paths_1.py
+++ leetcode/unique_paths_1.py
@@ -1,4 +1,6 @@
 class Solution:
+    def __init__(self):
+        self.lookup = dict()
     def uniquePaths(self, m, n):
         """
         :type m: int
@@ -7,16 +9,25 @@ class Solution:
         """
         if m == 1 or n == 1:
             return 1
-        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        elif m == 2:
+            return n
+        elif n == 2:
+            return m
+        if (m, n) in self.lookup:
+            return self.lookup[(m, n)]
+        else:
+            res = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+            self.lookup[(m, n)] = res
+            return res
 
 
 unique_paths = Solution().uniquePaths
 
-m28, n28 = 7, 3
 
-print('should be 28:')
-print(unique_paths(m28, n28))
+# m28, n28 = 7, 3
+# print('should be 28:')
+# print(unique_paths(m28, n28))
 
-m3, n3 = 3, 2
-print('should be 3:')
-print(unique_paths(m3, n3))
+# m3, n3 = 3, 2
+# print('should be 3:')
+# print(unique_paths(m3, n3))
